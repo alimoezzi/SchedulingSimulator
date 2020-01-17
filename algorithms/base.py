2fc3081ff2d93e0585becf2500e42a8f1f9d373c
s_arme <s.arme@ya.ru>
2020-01-18 01:33:11 +0330

    Implement main run method

--- algorithms/base.py
+++ algorithms/base.py
@@ -8,15 +8,30 @@ class BaseSch:
     def __init__(self, burstArray: list, priorityArray: list) -> None:
         self.burstArray = burstArray
         self.waitArray = [0 for i in burstArray]
-        self.comArray = [0 for i in burstArray]
+        self.comArray = burstArray
         self.processArray = [Worker(process) for i in range(len(burstArray))]
         self.q = Queue()
+        self.w = None
+        self.i = None
 
-    def pickNext(self):
+    def pickNext(self) -> (Worker, int):
         pass
 
     def run(self):
-        pass
+        while self.pickNext()[1] > 0:
+            (w, i) = self.pickNext()
+            if w != self.w:
+                self.w.pause()
+                self.w = Worker.start(i, self.burstArray)
+                self.i = i
+            sleep(0.001)
+            self.w.pause()
+            self.addAll(self.i)
+
+    def addAll(self, i):
+        for j in range(len(self.waitArray)):
+            if (j != i):
+                self.waitArray[j] += 1
 
 
 def process(t, i, b):
