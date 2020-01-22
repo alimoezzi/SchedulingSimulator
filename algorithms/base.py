bfdd019b22d9ce2c9712175304adbf58470e7408
s_arme <s.arme@ya.ru>
2020-01-22 19:24:35 +0330

    add process as a instance method instead of independent function

--- algorithms/base.py
+++ algorithms/base.py
@@ -1,40 +1,62 @@
-from queue import Queue
+from collections import deque
+from copy import deepcopy
 
-from worker import Worker
+from worker import Worker,create_worker
+from worker import sleep as wsleep
 from time import sleep
 
 
 class BaseSch:
-    def __init__(self, burstArray: list, priorityArray: list) -> None:
+    start = False
+    def __init__(self, arrivalArray: list, burstArray: list, priorityArray: list = None) -> None:
+        self.arrivalArray = arrivalArray
+        self.priorityArray = priorityArray
         self.burstArray = burstArray
         self.waitArray = [0 for i in burstArray]
-        self.comArray = burstArray
-        self.processArray = [Worker(process) for i in range(len(burstArray))]
-        self.q = Queue()
+        self.comArray = deepcopy(burstArray)
+        self.processArray = [(create_worker(self.process, index=i).pause(), i) for i in range(len(burstArray))]
+        self.q = deque()
         self.w = None
         self.i = None
+        self.init_queue()
 
-    def pickNext(self) -> (Worker, int):
+    def init_queue(self):
+        pass
+
+    def pick_next(self) -> (Worker, int):
         pass
 
     def run(self):
-        while self.pickNext()[1] > 0:
-            (w, i) = self.pickNext()
-            if w != self.w:
+        for i in range(len(self.burstArray)):
+            self.processArray[i][0].pause()
+        BaseSch.start = True
+        print('run')
+        while sum(self.burstArray) != 0:
+            w, i = self.pick_next()
+            if self.w is None and self.i is None:
+                self.w, self.i = w,i
+                self.w.resume()
+            elif self.burstArray[i] == 0:
+                print('stopping thread', i, 'remaining', sum(self.burstArray))
+                self.w.stop()
+            elif w != self.w:
                 self.w.pause()
-                self.w = Worker.start(i, self.burstArray)
-                self.i = i
+                self.w, self.i = w, i
+                self.w.resume()
             sleep(0.001)
             self.w.pause()
             self.addAll(self.i)
+            self.w.resume()
 
     def addAll(self, i):
         for j in range(len(self.waitArray)):
-            if (j != i):
-                self.waitArray[j] += 1
+            if j != i:
+                if self.burstArray[j] != 0:
+                    self.waitArray[j] += 1
 
-
-def process(t, i, b):
-    while b[i] > 0:
-        b[i] -= 1
-        sleep(0.001)
+    def process(self, index=None):
+        while self.burstArray[index] > 0:
+            if BaseSch.start:
+                print('\nthread' + str(index) + str(self.burstArray[index]) + '\n')
+                self.burstArray[index] -=1
+                wsleep(0.001)
